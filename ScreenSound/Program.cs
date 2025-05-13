607072de3d49c6925261a3aa77d75b2abef82310
Zenx007 <zenxpokas@gmail.com>
2025-05-13 13:39:19 -0300

    code refactor

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -2,28 +2,8 @@
 using ScreenSound.Menus;
 using ScreenSound.Modelos;
 
-try
-{
-    var connection = new  Connection();
-    var listarArtistas = connection.Listar();
-
-    foreach (var artista in listarArtistas)
-    {
-        Console.WriteLine(artista);
-    }
-}
-
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
-}
-Artista ira = new Artista("Ira!", "Banda Ira!");
-Artista beatles = new("The Beatles", "Banda The Beatles");
-
-Dictionary<string, Artista> artistasRegistrados = new();
-artistasRegistrados.Add(ira.Nome, ira);
-artistasRegistrados.Add(beatles.Nome, beatles);
-
+var context = new ScreenSoundContext(); 
+var artistaDAL = new ArtistaDAL(context);
 Dictionary<int, Menu> opcoes = new();
 opcoes.Add(1, new MenuRegistrarArtista());
 opcoes.Add(2, new MenuRegistrarMusica());
@@ -61,7 +41,7 @@ void ExibirOpcoesDoMenu()
     if (opcoes.ContainsKey(opcaoEscolhidaNumerica))
     {
         Menu menuASerExibido = opcoes[opcaoEscolhidaNumerica];
-        menuASerExibido.Executar(artistasRegistrados);
+        menuASerExibido.Executar(artistaDAL);
         if (opcaoEscolhidaNumerica > 0) ExibirOpcoesDoMenu();
     }
     else
